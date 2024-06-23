9afc3f579b6b024c5a1ed5d594d5c0ca742066c4
Alexey Lukashev <newrenoresident@gmail.com>
2024-06-23 12:43:38 +0300

    fixes2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/music-component-internship-vk.git/",
+  base: "/music-component-internship-vk/",
   plugins: [react()],
 });
